c56a94187131eef35141c7c543e46439dff2401c
moriarty <56692142+SundayG@users.noreply.github.com>
2021-01-07 18:29:17 +0800

    增加UIController类，实现放硬币

--- show.py
+++ show.py
@@ -1,22 +1,30 @@
 from PIL import Image
 
 
-def read_coin(img_name: str) -> Image.Image:
-    coin = Image.open(f"imgs/{img_name}")
-    coin = coin.resize((78, 78))
-    return coin
+class UIController(object):
+    def __init__(self):
+        self.background = Image.open("imgs/background.png")
 
+    def set_coin(self, coin_path: str, pos: tuple):
+        coin = UIController.__read_coin(img_name=coin_path)
+        self.background.paste(coin, pos, coin)
 
-def set_coin(coin: Image.Image, pos: tuple, bg: Image.Image):
-    bg.paste(coin, pos, coin)
+    def show(self):
+        self.background.show()
+
+    @staticmethod
+    def __read_coin(img_name: str) -> Image.Image:
+        coin = Image.open(f"imgs/{img_name}")
+        coin = coin.resize((78, 78))
+        return coin
 
 
 def main():
-    coin = read_coin("ppoint-removebg-preview.png")
-    bg = Image.open("imgs/background.png")
-    set_coin(coin=coin, pos=(309, 534), bg=bg)
-    set_coin(coin=coin, pos=(543, 578), bg=bg)
-    bg.show()
+    UI = UIController()
+    UI.set_coin(coin_path="ppoint-removebg-preview.png", pos=(309, 534))
+    UI.set_coin(coin_path="ppoint-removebg-preview.png", pos=(543, 578))
+
+    UI.show()
 
 
 if __name__ == '__main__':
